0053e9940acee1420c80e9210643cafd6382bf9d
tle0999 <panjatape.kr.66@ubu.ac.th>
2025-09-11 17:16:39 +0700

    อัพเดทเวอร์ชั่นล่าสุด

---
+++ Assets/Scripts/slimecontroller.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.AI;   
+
+public class slimecontroller : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject player;
+
+    public float stopDistance = 1.0f;   
+    public float detectionRange = 10.0f;
+
+    Animator anim;
+    bool isPlayerInRange = false;
+    bool hasReachedPlayer = false;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();    
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        isPlayerInRange = distance <= detectionRange;
+        if (isPlayerInRange)
+        {
+            if (distance <= stopDistance)
+            {
+                StopMoving();
+                hasReachedPlayer = true;
+            }
+            else
+            {
+                MoveToPlayer();
+            }
+        }
+        else
+        {
+            StopMoving();   
+        }              
+        UpdateAnimation();  
+    }
+    void MoveToPlayer()
+    {
+        if (agent.is)
+        {
+            agent.SetDestination(player.transform.position);
+            hasReachedPlayer = false;
+        }
+            
+    }
+
+    void StopMoving()
+    {
+            if (agent.isActiveAndEnabled)
+            {
+                agent.ResetPath();
+                hasReachedPlayer = true;
+            }
+    }
+    void UpdateAnimation()
+    {
+            anim.SetBool("isRunning", false);
+    }
+}
